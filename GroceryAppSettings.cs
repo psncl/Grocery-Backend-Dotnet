dcc794fa6f702b139bb6e1cc3006da102f527006
psncl <ps@ps.ncl>
2025-02-02 20:53:20 +0000

    Add config and class for dynamic price processor options

---
+++ GroceryAppSettings.cs
@@ -0,0 +1,6 @@
+namespace Backend;
+public class GroceryAppSettings
+{
+    public int PriceLoyaltyCard { get; set; }
+    public int DiscountLoyaltyCard { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
+using Backend;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+var appSettings = config.GetSection("GroceryAppSettings").Get<GroceryAppSettings>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => $"Hello World");
 
 app.Run();
