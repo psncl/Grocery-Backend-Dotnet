de6e014a0a7ba1b70762cc4ce15e492b385eaa2f
psncl <ps@ps.ncl>
2025-02-02 13:38:44 +0000

    Add initial models

---
+++ Models/GroceryItemModel.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Models
+{
+    public class GroceryItem
+    {
+
+        public string Name { get; set; }
+        public string BrandName { get; set; }
+        public decimal Price { get; set; }
+        public bool IsPhysical { get; set; }
+
+        public GroceryItem(string name, string brandName, decimal price, bool isPhysical = true)
+        {
+            Name = name;
+            BrandName = brandName;
+            Price = price;
+            IsPhysical = isPhysical;
+        }
+
+        public decimal GetCost(int quantity)
+        {
+            return Price * quantity;
+        }
+
+    }
+}
