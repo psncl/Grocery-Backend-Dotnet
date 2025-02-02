33ca205a7ad20cc8253e65ffe6337403817a4475
psncl <ps@ps.ncl>
2025-02-02 14:25:47 +0000

    Update GroceryOrder model

--- Models/GroceryOrderModel.cs
+++ Models/GroceryOrderModel.cs
@@ -16,23 +16,32 @@ namespace Backend.Models
         {
             OrderNumber = orderNumber;
             OrderedItems = new();
-            ShippingAddress = new ShippingInfo("", "", "");
+            ShippingAddress = new("", "", "");
         }
 
         public void AddItemToOrder(GroceryItem item, int quantity)
         {
             if (OrderedItems.ContainsKey(item))
             {
-                throw new InvalidOperationException("Cannot add same item to the order twice");
+                throw new InvalidOperationException($"{item.Name} already exists in the order. Cannot add it again.");
             }
 
-            if (quantity < 1)
+            if (quantity <= 0)
             {
-                throw new ArgumentOutOfRangeException($"Quantity must be greater than 0. Provided value: {quantity}");
+                throw new ArgumentOutOfRangeException($"Quantity must be greater than 0. Provided value: {quantity}.");
             }
 
             OrderedItems.Add(item, quantity);
         }
 
+        public decimal GetTotalCost()
+        {
+            decimal sum = 0;
+            foreach (KeyValuePair<GroceryItem, int> entry in OrderedItems)
+            {
+                sum += entry.Key.GetCost(entry.Value);
+            }
+            return sum;
+        }
     }
 }
